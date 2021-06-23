cf3d67d8ab295a1efa6b54b0a1eb18619202378d
alex <arodriguez202020@gmail.com>
2021-06-23 14:32:06 -0500

    add validaters to user model and restful routes in users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,8 +20,30 @@ class UsersController < ApplicationController
     if user.save
       render json: user
     else
-      render json: {error: product.errors.full_messages}, status :unprocessable_entity
+      render json: {error: user.errors.full_messages}, status: :unprocessable_entity
     end
+  end
+
+  def update
+    user_id = params["id"]
+    user = User.find_by(id: user_id)
+
+    user.first_name = params["first_name"] || user.first_name
+    user.last_name = params["last_name"] || user.last_name
+    user.email = params["email"] || user.email
+
+    if user.save
+      render json: user
+    else
+      render json: {error: user.errors.full_messages}, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    user_id = params["id"]
+    user = User.find_by(id: user_id)
+    user.destroy
+    render json: {message: "product successfully deleted."}
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+user = User.create(first_name: "John", last_name: "Stockton", email: "jstock@gmail.com")
+
+user = User.create(first_name: "Charles", last_name: "Barkely", email: "cbark@gmail.com")
+
+user = User.create(first_name: "Scottie", last_name: "Pippen", email: "spip@gmail.com")
